e7e54a71771d40e3a6cab88fd424f8cf4519ff04
Taufiq kurnia <63175313+thecrusader97@users.noreply.github.com>
2022-08-12 22:34:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Create dynamic CheckBox in PHP using MySQL </title>
+</head>
+
+<body>
+
+<?php
+
+include 'config.php';
+
+
+?>
+<h2> Dynamic CheckBox </h2>
+ 
+ <form method="post" action=" ">
+ 
+ <?php
+ $get_data = mysqli_query($conn,"SELECT * FROM checkbox");
+ while($row = mysqli_fetch_array($get_data)) {
+ echo "<input type='checkbox' value='{$row['Value']}'>" . $row['Name'] . '</br>';
+}
+
+?>
+ 
+ </form>
+
+</body>
+</html>
